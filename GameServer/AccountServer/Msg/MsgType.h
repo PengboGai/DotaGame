dbafff9c8a4a099195ad5dfee11b80dfde5baa61
binbin88115 <lansey.ax@gmail.com>
2014-08-19 19:56:54 +0800

    Revert "add project"
    This reverts commit 56c734ddf6f19a5018181169c0fd84ab104143ce.

--- GameServer/AccountServer/Msg/MsgType.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MESSAGE_TYPE_H_
-#define _MESSAGE_TYPE_H_
-
-#define MSG_ACCOUNT_LOGIN_C						1000	// 玩家登录（客户端->服务端）
-#define MSG_ACCOUNT_LOGIN_S						1001	// 玩家登录（服务端->客户端）
-#define MSG_ACCOUNT_REG_C						1002	// 玩家注册（客户端->服务端）
-#define MSG_ACCOUNT_REG_S						1003	// 玩家注册（服务端->客户端）
-
-#endif // _MESSAGE_TYPE_H_
